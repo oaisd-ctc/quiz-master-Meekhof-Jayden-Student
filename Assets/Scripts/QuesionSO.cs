780e69f74a0069f13feafae16650a32547c965b0
Meekhof-Jayden-Student <jame1ea@student.oaisd.org>
2023-03-20 10:33:46 -0400

    .

--- Assets/Scripts/QuesionSO.cs
+++ Assets/Scripts/QuesionSO.cs
@@ -7,5 +7,21 @@ public class QuesionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
 
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
 }
